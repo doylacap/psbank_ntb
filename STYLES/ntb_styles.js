33b4e400ae1166fdbbedeade66e682d9c1e54819
doylacap <oliverlacap1995@gmail.com>
2019-11-22 10:21:10 +0800

    updated design and terms/condition

--- STYLES/ntb_styles.js
+++ STYLES/ntb_styles.js
@@ -26,10 +26,29 @@ ntb_myriad_regular = 'myriad_pro_regular.ttf';
 
 export default styles = StyleSheet.create({
       Container: {
-        flex: 1, 
-        alignItems: 'center', 
         justifyContent: 'center',
-        backgroundColor: ntb_white
+        fontFamily: 'Avenir',
+        paddingHorizontal:wp('4.7%'),
+        display: 'flex',  
+      },
+      Container_Text: {
+        fontFamily: 'Avenir',
+        marginVertical:hp('1%'),
+        fontSize: wp('4%'),
+        textAlign:'justify'
+      },
+      Container_Title: {
+        fontFamily: 'Avenir',
+        marginVertical:hp('1%'),
+        fontSize: wp('5%'),
+        fontWeight:'bold',
+      },
+      Container_Title_Gray: {
+        fontFamily: 'Avenir',
+        marginVertical:hp('1%'),
+        fontSize: wp('5%'),
+        fontWeight:'bold',
+        color:'#999999'
       },
       Container_New: {
         flex: 1, 
@@ -78,12 +97,23 @@ export default styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'flex-start', 
         backgroundColor: 'transparent'
+      },    
+      Header:{
+        borderBottomColor:'#EE3031',
+        borderBottomWidth:15
       },
-      
+      Header_Title:{
+        color: ntb_white,
+        fontSize: wp('6%'),
+        textAlign: 'center',
+        alignSelf: 'center',
+        fontWeight: 'bold',
+        fontFamily: 'Avenir'
+      },
       ImageBgTop: {
         width: wp('100%'),
         height: hp ('22%'),
-        resizeMode: 'cover'
+        resizeMode: 'cover',
       },
 
       ButtonTextBlack: {
@@ -120,5 +150,69 @@ export default styles = StyleSheet.create({
         textAlign: 'left', alignSelf: 'stretch',
         fontFamily: 'Avenir'
       },
+      Text_Red: {
+        paddingLeft: wp('3%'),
+        paddingRight: wp('2.5%'),
+        fontSize: wp('3.8%'),
+        color : ntb_red,
+        textAlign: 'left', alignSelf: 'stretch',
+        fontFamily: 'Avenir'
+      },
+      Modal_Container: {
+        width: wp('90%'), 
+        height: hp('39%'),
+        justifyContent: 'center',
+        backgroundColor: ntb_white,
+        paddingHorizontal:wp('5%'),
+        borderRadius:10,
+        fontFamily: 'Avenir'
+      },
+      Text_Red_Modal_Title: {
+        fontSize: wp('5.8%'),
+        color : ntb_red,
+        fontWeight: 'bold',
+        textAlign: 'center', alignSelf: 'stretch',
+        fontFamily: 'Avenir',
+      },
+      Modal_Title_Horizontal_Line:{
+        borderBottomColor:'#E0E1E1',
+        borderBottomWidth:1,
+        paddingVertical: 5
+      },
+      Modal_Content: {
+        fontFamily: 'Avenir',
+        fontSize: wp('3.7%'),
+        textAlign:'justify',
+        paddingVertical:wp('2%')
+      },
+      ModalButtonContainer: {
+        width: wp('80%'), 
+        height: hp('5.5%'),
+        borderRadius: 15,
+        marginTop: hp('2%'),
+        backgroundColor: ntb_blue,
+      },
+
+      ModalButtonText: {
+        color: ntb_white,
+        fontWeight: 'bold',
+        alignSelf: 'center', 
+        fontSize: wp('4.1%'),
+        fontFamily: 'Avenir',
+        paddingTop: hp('1.5%'),
+      },
+      Terms_And_Condition_Send_Email_Container:{
+        backgroundColor:'#E0E1E1',
+        paddingVertical: hp('1%'),
+        paddingHorizontal: wp('2%'),
+        display: 'flex',  
+        flexDirection: 'row-reverse',
+        alignItems: 'center'
+      },
+      Terms_And_Condition_Send_Email_Text:{
+        fontFamily: 'Avenir',
+        fontSize: wp('3.5%'),
+        paddingHorizontal: wp('2%'),
+      }
 
   });
